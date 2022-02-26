5e4c079645d156d19d941b01b9f90a39b0538a61
Ahmed Ibrahim <ebrahema89859@gmail.com>
2022-02-26 23:17:37 +0200

    Add project files.

---
+++ RepositoryPatternWithUOW.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using RepositoryPatternWithUOW.Core.Consts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternWithUOW.Core.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+
+
+
+
+
+
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        IEnumerable<T> AddRange(IEnumerable<T> entities);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+        void DeleteRange(IEnumerable<T> entities);
+        void Attach(T entity);
+        void AttachRange(IEnumerable<T> entities);
+        int Count();
+        int Count(Expression<Func<T, bool>> criteria);
+        Task<int> CountAsync();
+        Task<int> CountAsync(Expression<Func<T, bool>> criteria);
+
+
+
+
+        T Find(Expression<Func<T, bool>> criteria, string[] includes = null);
+        Task<T> FindAsync(Expression<Func<T, bool>> criteria, string[] includes = null);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, string[] includes = null);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int take, int skip);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int? take, int? skip,
+            Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending);
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[] includes = null);
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, int skip, int take);
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, int? skip, int? take,
+            Expression<Func<T, object>> orderBy = null, string orderByDirection = OrderBy.Ascending);
+    }
+}
